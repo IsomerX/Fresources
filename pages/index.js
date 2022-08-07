49046c7fb5ae1c0d1c2544bf5326ba5de6de826a
notsosimppole <gitansh1208@gmail.com>
2022-08-07 23:29:58 +0530

    asd

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import PdfViewer from "../components/PdfViewer";
+import PdfViewer from '../components/PdfViewer'
 
 export default function Home() {
-    return <div className = "w-1/2 h-screen mx-auto overflow-y-scroll ">
-        <PdfViewer link = "https://data-storage.sgp1.digitaloceanspaces.com/H.C.%20TNEJA%201-500%20(1).pdf" />
-    </div>
+    return (
+        <div className="w-1/2 h-screen mx-auto overflow-y-scroll ">
+            <PdfViewer link="https://data-storage.sgp1.digitaloceanspaces.com/H.C.%20TNEJA%201-500%20(1).pdf" />
+        </div>
+    )
 }
-
