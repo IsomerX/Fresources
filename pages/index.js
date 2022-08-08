b1c111a4a4f33c92cfbcc1670857834b6a421575
Monarch <vamsivishwanath@gmail.com>
2022-08-08 13:42:51 +0530

    clean slate for branch vamsi: yarn into issues

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React from 'react'
 
-export default function Home() {}
+const index = () => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default index
