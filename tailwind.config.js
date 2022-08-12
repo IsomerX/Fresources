3118966c10437b0f7ac4454bba13adfffb17c388
Monarch <92059211+Monarch47@users.noreply.github.com>
2022-08-12 23:34:28 +0530

    pre commit: changes made; deletion

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,31 +3,7 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
     ],
-    // theme: {
-    //     extend: {
-    //         colors: {
-    //             babli: '#11101D',
-    //             babu: '#2c2859',
-    //             bablu: '#020221',
-    //             babla: "#0e0e12",
-    //             primary: {
-    //                 bablu: '#101033',
 
-    //                 black: '#0c0c0c',
-    //                 white: '#ffffff',
-    //                 pink: '#ff2bc1',
-    //             },
-    //             muted: {
-    //                 slate: '#75899c',
-    //             },
-    //             accent: {
-    //                 green: '#367737',
-    //                 purple: '#351f7f',
-    //                 magenta: '#5d174c',
-    //             },
-    //         },
-    //     },
-    // },
     theme: {
         extend: {
             fontFamily: {
