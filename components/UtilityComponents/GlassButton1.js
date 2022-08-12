be50ad105b5b01ddd29f005e6574c6cb5eb75d17
Monarch <92059211+Monarch47@users.noreply.github.com>
2022-08-12 23:06:48 +0530

    true commit: progress on UI

--- components/UtilityComponents/GlassButton1.js
+++ components/UtilityComponents/GlassButton1.js
@@ -4,8 +4,7 @@ import React from 'react'
 const GlassButton1 = (props) => {
     return (
         <Link href={props.target}>
-            <button className=
-                'px-8 py-4  md:py-4 md:px-6 rounded-xl font-bold text-2xl glass shadow-glass-2 shadow-[rgba(255,255,255,0.50)] hover:bg-gradient-to-b hover:from-highlights-blue duration-300 transition'>
+            <button className='px-8 py-4  md:py-4 md:px-6 rounded-xl font-bold text-2xl glass shadow-glass-2 shadow-[rgba(255,255,255,0.50)] hover:bg-gradient-to-b hover:from-highlights-blue duration-300 transition'>
                 {props.textContent}
             </button>
         </Link>
